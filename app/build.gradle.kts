792f11526f4f988c528ce40818ba7dc786b202d2
Andreea <andreea.matei123@yahoo.com>
2024-05-13 18:58:55 +0300

    Added realtime database

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,9 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
 }
