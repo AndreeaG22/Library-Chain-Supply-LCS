d4afdb0ad3acb2b3aa3b4d1289fcf37ef386c623
ioan teodorescu <94006639+iioan@users.noreply.github.com>
2024-05-14 11:24:15 +0300

    drawer

--- app/src/main/java/com/teamapp/lcs/MainActivity.kt
+++ app/src/main/java/com/teamapp/lcs/MainActivity.kt
@@ -2,13 +2,19 @@ package com.teamapp.lcs
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,9 +25,14 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+//        val database = Firebase.database
 
-        myRef.setValue("Hello, World!")
+        drawerLayout = findViewById(R.id.main)
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close)
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
