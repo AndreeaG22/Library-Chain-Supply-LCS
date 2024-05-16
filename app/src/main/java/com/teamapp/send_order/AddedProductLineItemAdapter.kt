f98de468f9b80fc79f10933cb3f14d10d2055fcc
Andreea <andreea.matei123@yahoo.com>
2024-05-17 00:46:43 +0300

    Added product lists for order

---
+++ app/src/main/java/com/teamapp/send_order/AddedProductLineItemAdapter.kt
@@ -0,0 +1,53 @@
+package com.teamapp.send_order
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.teamapp.lcs.databinding.AddedProductLineItemBinding
+
+class AddedProductLineItemAdapter (
+private val onItemDeleted: (item: Product) -> Unit
+): ListAdapter<Product, RecyclerView.ViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return AddedProductLnItmViewHolder(
+            AddedProductLineItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as AddedProductLnItmViewHolder).bind(getItem(position))
+    }
+
+    inner class AddedProductLnItmViewHolder(val binding: AddedProductLineItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Product) {
+            binding.apply {
+                prod.text = item.name
+
+                price.visibility = View.VISIBLE
+                price.text = item.price.toString()
+
+                qty.visibility = View.VISIBLE
+                qty.text = item.quantity.toString()
+
+            }
+        }
+    }
+
+    object DiffCallback : DiffUtil.ItemCallback<Product>() {
+        override fun areItemsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.name == newItem.name
+        }
+
+        override fun areContentsTheSame(oldItem: Product, newItem: Product): Boolean {
+            return oldItem.name == newItem.name
+        }
+    }
+}
--- app/src/main/java/com/teamapp/send_order/SendOrderFragment.kt
+++ app/src/main/java/com/teamapp/send_order/SendOrderFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.teamapp.home.HomeFragment
 import com.teamapp.lcs.R
 import com.teamapp.lcs.databinding.FragmentSendOrderBinding
@@ -15,6 +16,9 @@ class SendOrderFragment : Fragment() {
 
     private var _binding: FragmentSendOrderBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var addedProductLineItemAdapter: AddedProductLineItemAdapter
+    private lateinit var productLineItemAdapter: ProductLineItemAdapter
 
 
     companion object {
@@ -34,14 +38,57 @@ class SendOrderFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initUI()
+        val productList = listOf(
+            Product("Caiet", 12.0),
+            Product("Pix", 15.0),
+            Product("Creion", 20.0),
+            Product("Radiera", 25.0),
+            Product("Creta", 30.0)
+        )
+        productLineItemAdapter.submitList(productList.toMutableList())
     }
 
     private fun initUI() {
+        var qty = 0
+        var totalPrice = 0.0
         binding.sendOrderButton.setOnClickListener {
             // Send order
             Toast.makeText(requireContext(), "Order sent!", Toast.LENGTH_SHORT).show()
             // go back to previous fragment go back to previous fragment
-            parentFragmentManager.popBackStack() // TODO: check if this works
+//            parentFragmentManager.popBackStack() // TODO: check if this works
+            // clear back stack and go to home fragment
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment.newInstance())
+                .commit()
         }
+
+        addedProductLineItemAdapter = AddedProductLineItemAdapter { item ->
+//            viewModel.removeProduct(item)
+        }
+
+        productLineItemAdapter = ProductLineItemAdapter {
+            val crtList = productLineItemAdapter.currentList
+
+            val checkedItems = crtList.filter { it.isChecked }
+            addedProductLineItemAdapter.submitList(checkedItems.toMutableList())
+            qty = checkedItems.size
+            totalPrice = checkedItems.sumByDouble { it.price }
+
+            binding.textView4.text = totalPrice.toString()
+            binding.textView3.text = qty.toString()
+        }
+
+
+
+        binding.productsToAdd.apply {
+            adapter = productLineItemAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
+        binding.addedProducts.apply {
+            adapter = addedProductLineItemAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+
     }
 }
