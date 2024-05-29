8d5d5b9f840f1338e9d0c2743f9489761ea0e927
matei <mateiioan.dudu@gmail.com>
2024-05-29 17:10:19 +0300

    Now a deleted user cannot log back in

--- app/src/main/java/com/teamapp/lcs/LoginActivity.kt
+++ app/src/main/java/com/teamapp/lcs/LoginActivity.kt
@@ -1,19 +1,20 @@
 package com.teamapp.lcs
 
-
-
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.teamapp.lcs.databinding.ActivityLoginBinding
 import com.teamapp.lcs.databinding.ErrorDialogBinding
 
-
 class LoginActivity : AppCompatActivity() {
-
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
@@ -29,22 +30,36 @@ class LoginActivity : AppCompatActivity() {
             val pass = binding.passET.text.toString()
 
             if (email.isNotEmpty() && pass.isNotEmpty()) {
+                // check if the user exists in the realtime database
+                val db = Firebase.database
+                val query = db.getReference("employees").orderByChild("email").equalTo(email)
 
-                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
-                    if (it.isSuccessful) {
-                        val intent = Intent(this, MainActivity::class.java)
-                        intent.putExtra("email", email)
-                        startActivity(intent)
-                    } else {
-                        showErrorDialog("E-mail sau parola incorecte!")
+                query.addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            firebaseAuth.signInWithEmailAndPassword(email, pass)
+                                .addOnCompleteListener { task ->
+                                    if (task.isSuccessful) {
+                                        startActivity(Intent(this@LoginActivity, MainActivity::class.java))
+                                        finish()
+                                    } else {
+                                        showErrorDialog("Email sau parola gresita")
+                                    }
+                                }
+                        } else {
+                            showErrorDialog("Utilizatorul nu exista!")
+                        }
                     }
-                }
+
+                    override fun onCancelled(databaseError: DatabaseError) {
+                        showErrorDialog("Eroare la conectare!")
+                    }
+                })
             } else {
-                    showErrorDialog("Campurile nu pot fi goale")
+                showErrorDialog("Campurile nu pot fi goale")
             }
         }
     }
-
 
     private fun showErrorDialog(message: String) {
         Dialog(this).apply {
