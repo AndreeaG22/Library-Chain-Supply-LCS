83177514f668dfcb3e92bf45bf019ea0d21b8b3c
matei <mateiioan.dudu@gmail.com>
2024-05-30 20:57:51 +0300

    Made the receipt history live from the database

--- app/src/main/java/com/teamapp/ReceiptData.kt
+++ app/src/main/java/com/teamapp/ReceiptData.kt
@@ -9,4 +9,12 @@ class ReceiptData(
     val destination: String,
     val email: String) {
     constructor() : this(emptyList(), 0.0, "", "", "")
+
+    fun equals(other: ReceiptData): Boolean {
+        return this.products == other.products &&
+            this.totalPrice == other.totalPrice &&
+            this.date == other.date &&
+            this.destination == other.destination &&
+            this.email == other.email
+    }
 }
